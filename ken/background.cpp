99b0a8ce6ec13fa5d325e88b7784b3cbf3a09c7b
Ken Platz <ken.platz@netapp.com>
2018-05-14 16:41:43 -0600

    Updating

--- ken/background.cpp
+++ ken/background.cpp
@@ -18,8 +18,11 @@
 #include "lzo/lzo1a.h"
 #include "lzo/lzo1b.h"
 #include "lzo/lzo1x.h"
+#include "lz4/lz4.h"
 #include <bzlib.h>
+#include <lzma.h>
 #include <zlib.h>
+#include "snappy.h"
 
 using namespace std;
 void process_directory(const char*);
@@ -74,7 +77,6 @@ void process_file(const char* fname)
     int buflen;
     struct timeval tv1, tv2, tv3;
 
-    lzo_voidp lzo_wrkmem = malloc(65536L * lzo_sizeof_dict_t);
 
     gettimeofday(&tv1, nullptr);
     int fd = open(fname, O_RDONLY);
@@ -112,6 +114,7 @@ void process_file(const char* fname)
 
         if (compress_flags.lzo)
         {
+            lzo_voidp lzo_wrkmem = malloc(65536L * lzo_sizeof_dict_t);
             compress_flags.verbose && cout << "Compressing LZO1A" << endl;
 
             gettimeofday( &tv1, nullptr);
@@ -163,6 +166,7 @@ void process_file(const char* fname)
                 best = lcsize;
                 bname = "lzo1x-999";
             }
+            free(lzo_wrkmem);
         }
 
         if (compress_flags.gzip)
@@ -184,21 +188,129 @@ void process_file(const char* fname)
 
         if (compress_flags.bzip)
         {
-            gzsize = 2 * BLKSIZ;
+            csize = 2 * BLKSIZ;
             compress_flags.verbose && cout << "Compressing BZIP2" << endl;
             gettimeofday( &tv1, nullptr);
-            ret = BZ2_bzBuffToBuffCompress(outbuf, &gzsize, inbuf, BLKSIZ, 1, 0, 30);
+            ret = BZ2_bzBuffToBuffCompress(outbuf, &csize, inbuf, BLKSIZ, 1, 0, 30);
             gettimeofday( &tv2, nullptr);
             timersub( &tv2, &tv1, &tv3);
 
-            cout << "bzip2" << "|" << gzsize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
-            if (gzsize < best)
+            cout << "bzip2" << "|" << csize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            if (csize < best)
             {
-                best = gzsize;
+                best = csize;
                 bname = "bzip2";
             }
-
         }
+
+        if (compress_flags.lz4)
+        {
+            csize = 2 * BLKSIZ;
+            compress_flags.verbose && cout << "Compressing LZ4" << endl;
+            gettimeofday( &tv1, nullptr);
+            csize = LZ4_compress_default(inbuf, outbuf, BLKSIZ, 2*BLKSIZ);
+            gettimeofday( &tv2, nullptr);
+            timersub( &tv2, &tv1, &tv3);
+
+            cout << "lz4" << "|" << csize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            if (csize < best)
+            {
+                best = csize;
+                bname = "lz4";
+            }
+        }
+
+        if (compress_flags.snappy)
+        {
+            string output;
+
+            compress_flags.verbose && cout << "Compressing Snappy" << endl;
+            gettimeofday( &tv1, nullptr);
+            snappy::Compress(inbuf, BLKSIZ, &output);
+            csize = output.size();
+            gettimeofday( &tv2, nullptr);
+            timersub( &tv2, &tv1, &tv3);
+
+            cout << "snappy" << "|" << csize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            if (csize < best)
+            {
+                best = csize;
+                bname = "snappy";
+            }
+        }
+
+        if (compress_flags.lzma)
+        {
+            lzma_stream lzst = LZMA_STREAM_INIT;
+            if (lzma_easy_encoder(&lzst, 9, LZMA_CHECK_CRC64) != LZMA_OK)
+            {
+                switch(ret)
+                {
+                case LZMA_MEM_ERROR: 
+                    cerr << "LZMA Memory allocation failed" << endl;
+                    break;
+                case LZMA_OPTIONS_ERROR: 
+                    cerr << "LZMA Unsupported options" << endl;
+                    break;
+                case LZMA_UNSUPPORTED_CHECK:
+                    cerr << "LZMA integrity check unsupported" << endl;
+                    break;
+                default:
+                    cerr << "Unknown LZMA error during initialization" << endl;
+                    break;
+                }
+                continue;
+            } 
+            else 
+            {
+                lzst.next_in = (unsigned char*)inbuf;
+                lzst.avail_in = sizeof(inbuf);
+                lzst.next_out = (unsigned char*)outbuf;
+                lzst.avail_out = sizeof(outbuf);
+
+                gettimeofday(&tv1, nullptr);
+
+                do 
+                {
+                    ret = lzma_code(&lzst, LZMA_FINISH);
+                    if (ret != LZMA_OK)
+                    {
+                        switch(ret)
+                        {
+                        case LZMA_MEM_ERROR: 
+                            cerr << "LZMA Memory allocation failed" << endl;
+                            break;
+                        case LZMA_OPTIONS_ERROR: 
+                            cerr << "LZMA Unsupported options" << endl;
+                            break;
+                        case LZMA_UNSUPPORTED_CHECK:
+                            cerr << "LZMA integrity check unsupported" << endl;
+                            break;
+                        case LZMA_STREAM_END:
+                            // Normal when we've reached end of stream...
+                            break;
+                        default:
+                            cerr << "Unknown LZMA error during coding: " << ret << endl;
+                            break;
+                        }
+                    }
+                } 
+                while(lzst.avail_in > 0);
+                lzma_end(&lzst);
+
+                gettimeofday(&tv2, nullptr);
+                csize = lzst.total_out;
+                timersub( &tv2, &tv1, &tv3);
+
+                cout << "lzma" << "|" << csize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+                if (csize < best)
+                {
+                    best = csize;
+                    bname = "lzma";
+                }
+            }
+        }
+
         count++;
     }
 }
