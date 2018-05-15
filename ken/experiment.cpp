801d81c8e8649853c5be05ebfc224b2801c82e7d
Ken Platz <ken.platz@netapp.com>
2018-05-15 09:53:22 -0600

    Updating

--- ken/experiment.cpp
+++ ken/experiment.cpp
@@ -41,6 +41,7 @@ union {
         unsigned   lzma   : 1;
         unsigned   snappy : 1;
         unsigned   verbose : 1;
+        unsigned   best    : 1;
     };
 } compress_flags;
 
@@ -48,7 +49,7 @@ void process_directory(const char *dirname)
 {
     DIR* dhandle;
 
-    cout << "Processing directory: " << dirname << endl;
+    compress_flags.verbose && cout << "Processing directory: " << dirname << endl;
     if (NULL == (dhandle = opendir(dirname)))
     {
         cerr << "Unable to open directory " << dirname << ": " << strerror(errno) << endl;
@@ -71,13 +72,14 @@ void process_directory(const char *dirname)
 
 void process_file(const char* fname)
 {
-    cout << "Processing file: " << fname << endl;
+    compress_flags.verbose && cout << "Processing file: " << fname << endl;
     char inbuf[BLKSIZ];
     char outbuf[BLKSIZ*2];
+    char vbuf[BLKSIZ*2];
     int buflen;
     struct timeval tv1, tv2, tv3;
 
-
+    string msg;
     gettimeofday(&tv1, nullptr);
     int fd = open(fname, O_RDONLY);
     if (fd < 0)
@@ -89,31 +91,41 @@ void process_file(const char* fname)
     int count{1};
     while((buflen = read(fd, inbuf, BLKSIZ)) > 0)
     {
-        unsigned int csize, best = BLKSIZ;
+        unsigned int csize, best = BLKSIZ, vsize;
         long unsigned int gzsize;
         lzo_uint lcsize;
         int ret;
-        string bname;
+        string bname, buname;
         // Only compress full blocks.
         if (buflen < BLKSIZ) continue;
         compress_flags.verbose && cout << "Read block " << count << " of " << buflen << " bytes" << endl;
 
         if (compress_flags.lzf)
         {
+            ostringstream os;
             compress_flags.verbose && cout << "Compressing LZF..." << endl;
             gettimeofday( &tv1, nullptr);
             csize = lzf_compress(inbuf, BLKSIZ, outbuf, BLKSIZ - 1);
             gettimeofday( &tv2, nullptr);
 
             timersub( &tv2, &tv1, &tv3);
-
-            cout << "lzf|" << csize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            os << "lzf|" << csize << "|" << tv3.tv_usec;
             best = csize;
-            bname = "lzf";
+
+            gettimeofday(&tv1, nullptr);
+            vsize = lzf_decompress(outbuf, csize, vbuf, sizeof(vbuf));
+            gettimeofday(&tv2, nullptr);
+
+            timersub( &tv2, &tv1, &tv3 );
+            os << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            bname = os.str();
+            compress_flags.best || cout << os.str();
         }
 
         if (compress_flags.lzo)
         {
+            ostringstream os;
+            lzo_uint vsize;
             lzo_voidp lzo_wrkmem = malloc(65536L * lzo_sizeof_dict_t);
             compress_flags.verbose && cout << "Compressing LZO1A" << endl;
 
@@ -126,12 +138,23 @@ void process_file(const char* fname)
             gettimeofday( &tv2, nullptr);
             timersub( &tv2, &tv1, &tv3);
 
-            cout << "lzo1a|" << lcsize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            os << "lzo-1a|" << lcsize << "|" << tv3.tv_usec << "|";
+
+            gettimeofday( &tv1, nullptr);
+            lzo1a_decompress( (const lzo_bytep)outbuf, lcsize, (lzo_bytep)vbuf, &vsize, lzo_wrkmem);
+            gettimeofday( &tv2, nullptr);
+            timersub( &tv2, &tv1, &tv3);
+
+            os << tv3.tv_usec << "|" << fname << "|" << count << endl;
+
             if (lcsize < best)
             {
                 best = lcsize;
-                bname = "lzo1a";
+                bname = os.str();
             }
+            compress_flags.best || cout << os.str();
+
+            os.clear(), os.str(string{});
 
             compress_flags.verbose && cout << "Compressing LZO1X-1" << endl;
             gettimeofday( &tv1, nullptr);
@@ -143,51 +166,54 @@ void process_file(const char* fname)
             gettimeofday( &tv2, nullptr);
             timersub( &tv2, &tv1, &tv3);
 
-            cout << "lzo1x-1" << "|" << lcsize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
-            if (lcsize < best)
-            {
-                best = lcsize;
-                bname = "lzo1x-1";
-            }
+            os << "lzo1x-1" << "|" << lcsize << "|" << tv3.tv_usec << "|";
 
-            compress_flags.verbose && cout << "Compressing LZO1X-999" << endl;
             gettimeofday( &tv1, nullptr);
-            ret = lzo1x_999_compress((lzo_bytep) inbuf, 
-                                     (lzo_uint) BLKSIZ,
-                                     (lzo_bytep) outbuf, 
-                                     &lcsize,
-                                     lzo_wrkmem);
+            lzo1x_decompress((const lzo_bytep)outbuf, lcsize, (lzo_bytep)vbuf, &vsize, lzo_wrkmem);
             gettimeofday( &tv2, nullptr);
             timersub( &tv2, &tv1, &tv3);
 
-            cout << "lzo1x-999" << "|" << lcsize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            os << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            compress_flags.best || cout << os.str();
+            free(lzo_wrkmem);
+
             if (lcsize < best)
             {
                 best = lcsize;
-                bname = "lzo1x-999";
+                bname = os.str();
             }
-            free(lzo_wrkmem);
         }
 
         if (compress_flags.gzip)
         {
+            ostringstream os;
             gzsize = 2 * BLKSIZ;
+            auto gzsize2 = gzsize;
             compress_flags.verbose && cout << "Compressing DEFLATE(GZIP)" << endl;
             gettimeofday( &tv1, nullptr);
             ret = compress((unsigned char*)outbuf, &gzsize, (unsigned char*)inbuf, BLKSIZ);
             gettimeofday( &tv2, nullptr);
             timersub( &tv2, &tv1, &tv3);
 
-            cout << "deflate" << "|" << gzsize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            os << "deflate" << "|" << gzsize << "|" << tv3.tv_usec << "|";
+
+            gettimeofday( &tv1, nullptr);
+            ret = uncompress((unsigned char*)vbuf, &gzsize2, (unsigned char*)outbuf, gzsize);
+            gettimeofday( &tv2, nullptr);
+            timersub( &tv2, &tv1, &tv3);
+            os << tv3.tv_usec << "|" << fname << "|" << count << endl;
+
             if (gzsize < best)
             {
                 best = gzsize;
-                bname = "deflate";
+                bname = os.str();
             }
+            compress_flags.best || cout << os.str();
         }
 
         if (compress_flags.bzip)
         {
+            ostringstream os;
             csize = 2 * BLKSIZ;
             compress_flags.verbose && cout << "Compressing BZIP2" << endl;
             gettimeofday( &tv1, nullptr);
@@ -195,16 +221,26 @@ void process_file(const char* fname)
             gettimeofday( &tv2, nullptr);
             timersub( &tv2, &tv1, &tv3);
 
-            cout << "bzip2" << "|" << csize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            os << "bzip2" << "|" << csize << "|" << tv3.tv_usec << "|";
+
+            gettimeofday( &tv1, nullptr);
+            ret = BZ2_bzBuffToBuffDecompress(vbuf, &vsize, outbuf, csize, 0, 0);
+            gettimeofday( &tv2, nullptr);
+            timersub( &tv2, &tv1, &tv3);
+
+            os << tv3.tv_usec << "|" << fname << "|" << count << endl;
             if (csize < best)
             {
                 best = csize;
-                bname = "bzip2";
+                bname = os.str();
             }
+
+            compress_flags.best || cout << os.str();
         }
 
         if (compress_flags.lz4)
         {
+            ostringstream os;
             csize = 2 * BLKSIZ;
             compress_flags.verbose && cout << "Compressing LZ4" << endl;
             gettimeofday( &tv1, nullptr);
@@ -212,17 +248,26 @@ void process_file(const char* fname)
             gettimeofday( &tv2, nullptr);
             timersub( &tv2, &tv1, &tv3);
 
-            cout << "lz4" << "|" << csize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            os << "lz4" << "|" << csize << "|" << tv3.tv_usec << "|";
+
+            gettimeofday( &tv1, nullptr);
+            ret = LZ4_decompress_safe(outbuf, vbuf, csize, sizeof(vbuf));
+            gettimeofday( &tv2, nullptr);
+            timersub( &tv2, &tv1, &tv3);
+
+            os << tv3.tv_usec << "|" << fname << "|" << count << endl;
             if (csize < best)
             {
                 best = csize;
-                bname = "lz4";
+                bname = os.str();
             }
+            compress_flags.best || cout << os.str();
         }
 
         if (compress_flags.snappy)
         {
-            string output;
+            ostringstream os;
+            string output, verify;
 
             compress_flags.verbose && cout << "Compressing Snappy" << endl;
             gettimeofday( &tv1, nullptr);
@@ -231,16 +276,26 @@ void process_file(const char* fname)
             gettimeofday( &tv2, nullptr);
             timersub( &tv2, &tv1, &tv3);
 
-            cout << "snappy" << "|" << csize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            os << "snappy" << "|" << csize << "|" << tv3.tv_usec << "|";
+
+
+            gettimeofday( &tv1, nullptr);
+            ret = snappy::Uncompress(output.data(), output.size(), &verify);
+            gettimeofday( &tv2, nullptr);
+            timersub( &tv2, &tv1, &tv3);
+
+            os << tv3.tv_usec << "|" << fname << "|" << count << endl;
             if (csize < best)
             {
                 best = csize;
-                bname = "snappy";
+                bname = os.str();
             }
+            compress_flags.best || cout << os.str();
         }
 
         if (compress_flags.lzma)
         {
+            ostringstream os;
             lzma_stream lzst = LZMA_STREAM_INIT;
             if (lzma_easy_encoder(&lzst, 9, LZMA_CHECK_CRC64) != LZMA_OK)
             {
@@ -302,14 +357,63 @@ void process_file(const char* fname)
                 csize = lzst.total_out;
                 timersub( &tv2, &tv1, &tv3);
 
-                cout << "lzma" << "|" << csize << "|" << tv3.tv_usec << "|" << fname << "|" << count << endl;
-                if (csize < best)
-                {
-                    best = csize;
-                    bname = "lzma";
-                }
+                os << "lzma" << "|" << csize << "|" << tv3.tv_usec << "|";
             }
+            lzst = LZMA_STREAM_INIT;
+            if (lzma_stream_decoder(&lzst, UINT64_MAX, 0) != LZMA_OK)
+            {
+                cerr << "Error initializing LZMA decoder." << endl;
+                cout << "[error]" << endl;
+            }
+            else 
+            {
+                lzst.next_in = (unsigned char*)outbuf;
+                lzst.next_out = (unsigned char*)vbuf;
+                lzst.avail_in = csize;
+                lzst.avail_out = sizeof(vbuf);
+
+                gettimeofday(&tv1, nullptr);
+                do 
+                {
+                    ret = lzma_code(&lzst, LZMA_FINISH);
+                    if (ret != LZMA_OK)
+                    {
+                        switch(ret)
+                        {
+                        case LZMA_MEM_ERROR: 
+                            cerr << "LZMA Memory allocation failed" << endl;
+                            break;
+                        case LZMA_OPTIONS_ERROR: 
+                            cerr << "LZMA Unsupported options" << endl;
+                            break;
+                        case LZMA_UNSUPPORTED_CHECK:
+                            cerr << "LZMA integrity check unsupported" << endl;
+                            break;
+                        case LZMA_STREAM_END:
+                            // Normal when we've reached end of stream...
+                            break;
+                        default:
+                            cerr << "Unknown LZMA error during coding: " << ret << endl;
+                            break;
+                        }
+                    }
+                } 
+                while(lzst.avail_in > 0);
+                lzma_end(&lzst);
+
+                gettimeofday(&tv2, nullptr);
+                timersub(&tv2, &tv1, &tv3);
+
+                os << tv3.tv_usec << "|" << fname << "|" << count << endl;
+            }
+            if (csize < best)
+            {
+                best = csize;
+                bname = os.str();
+            }
+            compress_flags.best || cout << os.str();
         }
+        compress_flags.best && cout << bname;
 
         count++;
     }
@@ -345,6 +449,7 @@ int main(int argc, char** argv)
     compress_flags.summary = 0;
     struct option myopts[] = 
     {
+        {"best",    0, NULL, 'B'},
         {"bzip",    0, NULL, 'b'},
         {"gzip",    0, NULL, 'g'},
         {"lz4",     0, NULL, '4'},
@@ -356,11 +461,11 @@ int main(int argc, char** argv)
         {NULL, 0, NULL, 0}
     };
 
-    while(-1 != (opt = getopt_long(argc, argv, "v", myopts, &index)))
+    while(-1 != (opt = getopt_long(argc, argv, "Bbg4foasv", myopts, &index)))
     {
-        cout << "Got option: " << (char)opt << endl;
         switch(opt)
         {
+        case 'B': compress_flags.best    = true; break;
         case 'b': compress_flags.bzip    = true; break;
         case 'g': compress_flags.gzip    = true; break;
         case '4': compress_flags.lz4     = true; break;
@@ -374,7 +479,18 @@ int main(int argc, char** argv)
         }
     }
 
-    cout << "LZF: " << compress_flags.lzf << endl;
+    if (compress_flags.verbose) 
+    {
+        cout << "best:   " << compress_flags.best << endl;
+        cout << "bzip:   " << compress_flags.bzip << endl;
+        cout << "gzip:   " << compress_flags.gzip << endl;
+        cout << "LZ4:    " << compress_flags.lz4 << endl;
+        cout << "LZF:    " << compress_flags.lzf << endl;
+        cout << "LZO:    " << compress_flags.lzo << endl;
+        cout << "LZMA:   " << compress_flags.lzma << endl;
+        cout << "Snappy: " << compress_flags.lzma << endl;
+        cout << "Verbose:" << compress_flags.verbose << endl;
+    }
 
     for(int i=optind; i<argc; ++i)
     {
